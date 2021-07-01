14d2c4405104b106272cb1093581876fc68b1bf6
Juyeong11 <namjuyeong21@gmail.com>
2021-07-01 21:56:50 +0900

    DirectXTex
    texture processing library
    ScreebGrab-> d3d12 screenshots

--- 종합설계/6.25(동영상 저장 모듈)/Project60/GameFramework.h
+++ 종합설계/6.25(동영상 저장 모듈)/Project60/GameFramework.h
@@ -37,6 +37,7 @@ public:
 	void WaitForGpuComplete();
 	void MoveToNextFrame();
 
+	void OutPutBackBuffer();
 	void OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
 	void OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
 	LRESULT CALLBACK OnProcessingWindowMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
