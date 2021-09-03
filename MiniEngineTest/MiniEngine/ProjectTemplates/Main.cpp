8d20cda0af5d7b0a9057f3c9ada661dc4ef457d9
SH-Broar / Ephxod <ephxod01@naver.com>
2021-09-03 18:01:45 +0900

    Resolve Library Issue
    OK, Let's Start...

--- MiniEngineTest/MiniEngine/ProjectTemplates/Main.cpp
+++ MiniEngineTest/MiniEngine/ProjectTemplates/Main.cpp
@@ -1,24 +1,41 @@
-#include "pch.h"
+
 #include "GameCore.h"
-#include "GraphicsCore.h"
+#include "CameraController.h"
+#include "BufferManager.h"
+#include "Camera.h"
+#include "CommandContext.h"
+#include "TemporalEffects.h"
+#include "MotionBlur.h"
+#include "DepthOfField.h"
+#include "PostEffects.h"
+#include "SSAO.h"
+#include "FXAA.h"
 #include "SystemTime.h"
 #include "TextRenderer.h"
+#include "ParticleEffectManager.h"
 #include "GameInput.h"
-#include "CommandContext.h"
-#include "RootSignature.h"
-#include "PipelineState.h"
-#include "BufferManager.h"
+#include "SponzaRenderer.h"
+#include "glTF.h"
+#include "Renderer.h"
+#include "Model.h"
+#include "ModelLoader.h"
+#include "ShadowCamera.h"
+#include "Display.h"
+
+//#define LEGACY_RENDERER
 
 using namespace GameCore;
+using namespace Math;
 using namespace Graphics;
+using namespace std;
 
-class TEMPLATE_NAME : public GameCore::IGameApp
+using Renderer::MeshSorter;
+
+class BeatSlime : public GameCore::IGameApp
 {
 public:
 
-    TEMPLATE_NAME()
-    {
-    }
+    BeatSlime() {}
 
     virtual void Startup( void ) override;
     virtual void Cleanup( void ) override;
@@ -27,35 +44,66 @@ public:
     virtual void RenderScene( void ) override;
 
 private:
+
+    Camera m_Camera;
+    unique_ptr<CameraController> m_CameraController;
+
+    D3D12_VIEWPORT m_MainViewport;
+    D3D12_RECT m_MainScissor;
+
+    ModelInstance m_ModelInst;
+    ShadowCamera m_SunShadowCamera;
+
 };
 
-CREATE_APPLICATION( TEMPLATE_NAME )
+CREATE_APPLICATION(BeatSlime)
 
-void TEMPLATE_NAME::Startup( void )
+ExpVar g_SunLightIntensity("Viewer/Lighting/Sun Light Intensity", 4.0f, 0.0f, 16.0f, 0.1f);
+NumVar g_SunOrientation("Viewer/Lighting/Sun Orientation", -0.5f, -100.0f, 100.0f, 0.1f);
+NumVar g_SunInclination("Viewer/Lighting/Sun Inclination", 0.75f, 0.0f, 1.0f, 0.01f);
+
+//IBL æ»≥÷¿Ω
+
+#include <direct.h> // for _getcwd() to check data root path
+
+void BeatSlime::Startup( void )
 {
-    // Setup your data
+    //renderer init
+    Renderer::Initialize();
+
+    //file name
+    std::wstring gltfFileName;
+
+    //Camera Init (Reset)
+    m_Camera.SetZRange(1.0f, 10000.0f);
+    if (gltfFileName.size() == 0)
+        m_CameraController.reset(new FlyingFPSCamera(m_Camera, Vector3(kYUnitVector)));
+    else
+        m_CameraController.reset(new OrbitCamera(m_Camera, m_ModelInst.GetBoundingSphere(), Vector3(kYUnitVector)));
+
+
 }
 
-void TEMPLATE_NAME::Cleanup( void )
+void BeatSlime::Cleanup( void )
 {
     // Free up resources in an orderly fashion
 }
 
-void TEMPLATE_NAME::Update( float /*deltaT*/ )
+void BeatSlime::Update( float /*deltaT*/ )
 {
     ScopedTimer _prof(L"Update State");
 
     // Update something
 }
 
-void TEMPLATE_NAME::RenderScene( void )
+void BeatSlime::RenderScene( void )
 {
     GraphicsContext& gfxContext = GraphicsContext::Begin(L"Scene Render");
 
     gfxContext.TransitionResource(g_SceneColorBuffer, D3D12_RESOURCE_STATE_RENDER_TARGET, true);
     gfxContext.ClearColor(g_SceneColorBuffer);
     gfxContext.SetRenderTarget(g_SceneColorBuffer.GetRTV());
-    gfxContext.SetViewportAndScissor(0, 0, g_SceneColorBuffer.GetWidth(), g_SceneColorBuffer.GetHeight());
+    gfxContext.SetViewportAndScissor(0, 0, g_SceneColorBuffer.GetWidth(), g_SceneColorBuffer.GetHeight()); 
 
     // Rendering something
 
