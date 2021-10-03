47e382960482a2721b0c8c695ab7c856cc8fdeae
Ju yeong <namjuyeong21@gmail.com>
2021-10-03 18:20:02 +0900

    SoundManager SpeakerEffect

---
+++ BeatSlimeClient/Assets/Scripts/Sound/SoundEffectManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectManager : MonoBehaviour
+{
+    [SerializeField] Animator LeftSpeakerAnimator = null;
+    [SerializeField] Animator RightSpeakerAnimator = null;
+    string beat = "Beat";
+
+    public void BeatEffect()
+    {
+        LeftSpeakerAnimator.SetTrigger(beat);
+        RightSpeakerAnimator.SetTrigger(beat);
+    }
+}
