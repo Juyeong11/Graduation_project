e7bab9db00d178c67d2b91c7d70cb8e761b360f1
SH-Broar / Ephxod <ephxod01@naver.com>
2021-11-20 13:12:41 +0900

    HitMaker v.1
    Test Scene and Sandbox folder

--- BeatSlimeClient/Assets/Scenes/Sandbox/SANDBOXSoundTestProj.cs
+++ BeatSlimeClient/Assets/Scenes/Sandbox/SANDBOXSoundTestProj.cs
@@ -7,43 +7,48 @@ public class SANDBOXSoundTestProj : MonoBehaviour
 {
     public AudioMixer masterMixer;
     public float sin;
+    public float speeds;
     bool collapse = false;
     // Start is called before the first frame update
     void Start()
     {
         sin = 0.1f;
+        speeds = 0.5f;
     }
 
     public void SetSin()
     {
         masterMixer.SetFloat("SinDelta", sin);
+        masterMixer.SetFloat("Blend", speeds);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (!collapse)
         {
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                sin += 0.01f;
-                SetSin();
+            sin -= 0.1f;
+            SetSin();
+
+            if (sin < 0f)
                 collapse = true;
-            }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                sin -= 0.01f;
-                SetSin();
-                collapse = true;
-            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            collapse = false;
+            sin = speeds + 0.3f;
         }
         if (Input.GetKeyUp(KeyCode.UpArrow))
         {
-            collapse = false;
+            speeds += 0.1f;
+            if (speeds > 1f)
+                speeds = 1f;
         }
         if (Input.GetKeyUp(KeyCode.DownArrow))
         {
-            collapse = false;
+            speeds -= 0.1f;
+            if (speeds < 0f)
+                speeds = 0f;
         }
     }
 }
