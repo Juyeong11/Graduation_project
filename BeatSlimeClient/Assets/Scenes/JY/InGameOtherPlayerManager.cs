d5bac97b7ee8c557e705d315944f57e9a46a64b0
Ju yeong <namjuyeong21@gmail.com>
2022-01-17 00:39:05 +0900

    장판 서버 클라 동기화

--- BeatSlimeClient/Assets/Scenes/JY/InGameOtherPlayerManager.cs
+++ BeatSlimeClient/Assets/Scenes/JY/InGameOtherPlayerManager.cs
@@ -2,13 +2,8 @@ using UnityEngine;
 using UnityEngine.Events;
 using System.Collections.Generic;
 
-public enum playerState
-{
-    Idle,
-    move,
-}
 
-public class PlayerManager : MonoBehaviour
+public class InGameOtherPlayerManager : MonoBehaviour
 {
     public Animator JumpTrigger;
     public UnityEvent onPlayerStand;
@@ -26,16 +21,11 @@ public class PlayerManager : MonoBehaviour
 
     public Transform PlayerTransform;
 
-    bool playerAttacking;
-    List<(Beat,float)> SettledBallBeats;
-
     public void Start()
     {
-        playerAttacking = false;
         grid = GameManager.data.grid;
         state = playerState.Idle;
         selfDirection = HexDirection.Up;
-        SettledBallBeats = new List<(Beat,float)>();
         //onPlayerFly.Invoke();
         isFly = true;
     }
@@ -77,13 +67,8 @@ public class PlayerManager : MonoBehaviour
 
         if (GameManager.data.isGameStart)
         {
-            playerAttacking = false;
-
-            KeyHandler();
-            PlayerRotateToLookAt();
             PlayerWCheck();
-            PlayerPortalCheck();
-            BallBeatCheck();
+            //PlayerPortalCheck();
         }
     }
     public void PlayerPortalCheck()
@@ -112,8 +97,9 @@ public class PlayerManager : MonoBehaviour
     {
         if (selfCoord.coordinates.W != grid.cellMaps.Get(selfCoord.coordinates).w + 1)
         {
-            Debug.LogError(">Player W Coordinate Error!< [ Self W : " + selfCoord.coordinates.W + ", Cell W : " + grid.cellMaps.Get(selfCoord.coordinates).w + 1 + " ]");
-            //print("Self W : " + selfCoord.coordinates.W);
+            Debug.LogError(">Player W Coordinate Error!<");
+            print("Self W : " + selfCoord.coordinates.W);
+            print("Self W : " + grid.cellMaps.Get(selfCoord.coordinates).w);
             selfCoord.coordinates.W = grid.cellMaps.Get(selfCoord.coordinates).w + 1;
         }
         PlayerTransform.position = selfCoord.calculatePlayerPosition();
@@ -182,11 +168,7 @@ public class PlayerManager : MonoBehaviour
                 break;
         }
 
-        if (GameManager.data.getIsGoodTiming() != 0)
-        {
             return true;
-        }
-        return false;
     }
     public void PlayerMove(int x, int y, int z)
     {
@@ -197,24 +179,19 @@ public class PlayerManager : MonoBehaviour
     }
     void KeyHandler()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            playerAttacking = true;
-        }
-
         if (Input.GetKeyDown(KeyCode.Q) && KeyCheck(KeyCode.Q))
         {
             if (FieldGameManager.Net.isOnline)
             {
                 //Debug.Log("키 전송");
                 // 서버에 이동 전송
-                GameManager.data.setMoved();
+                //FieldGameManager.data.setMoved();
 
                 FieldGameManager.Net.SendMovePacket((byte)Protocol.DIR.LEFTUP);
             }
             else
             {
-                GameManager.data.setMoved();
+                //FieldGameManager.data.setMoved();
                 if (grid.cellMaps.Get(selfCoord.coordinates.X - 1, selfCoord.coordinates.Y, selfCoord.coordinates.Z + 1).state != cellState.None)
                 {
                     if (grid.cellMaps.Get(selfCoord.coordinates.X - 1, selfCoord.coordinates.Y, selfCoord.coordinates.Z + 1).w <= selfCoord.coordinates.W)
@@ -229,13 +206,13 @@ public class PlayerManager : MonoBehaviour
         {
             if (FieldGameManager.Net.isOnline)
             {
-                GameManager.data.setMoved();
+                //GameManager.data.setMoved();
                 // 서버에 이동 전송
                 FieldGameManager.Net.SendMovePacket((byte)Protocol.DIR.UP);
             }
             else
             {
-                GameManager.data.setMoved();
+                //GameManager.data.setMoved();
                 if (grid.cellMaps.Get(selfCoord.coordinates.X, selfCoord.coordinates.Y - 1, selfCoord.coordinates.Z + 1).state != cellState.None)
                 {
                     if (grid.cellMaps.Get(selfCoord.coordinates.X, selfCoord.coordinates.Y - 1, selfCoord.coordinates.Z + 1).w <= selfCoord.coordinates.W)
@@ -250,13 +227,13 @@ public class PlayerManager : MonoBehaviour
         {
             if (FieldGameManager.Net.isOnline)
             {
-                GameManager.data.setMoved();
+                //GameManager.data.setMoved();
                 // 서버에 이동 전송
                 FieldGameManager.Net.SendMovePacket((byte)Protocol.DIR.RIGHTUP);
             }
             else
             {
-                GameManager.data.setMoved();
+                //FieldGameManager.data.setMoved();
                 if (grid.cellMaps.Get(selfCoord.coordinates.X + 1, selfCoord.coordinates.Y - 1, selfCoord.coordinates.Z).state != cellState.None)
                 {
                     if (grid.cellMaps.Get(selfCoord.coordinates.X + 1, selfCoord.coordinates.Y - 1, selfCoord.coordinates.Z).w <= selfCoord.coordinates.W)
@@ -272,13 +249,13 @@ public class PlayerManager : MonoBehaviour
         {
             if (FieldGameManager.Net.isOnline)
             {
-                GameManager.data.setMoved();
+                //GameManager.data.setMoved();
                 // 서버에 이동 전송
                 FieldGameManager.Net.SendMovePacket((byte)Protocol.DIR.LEFTDOWN);
             }
             else
             {
-                GameManager.data.setMoved();
+                //FieldGameManager.data.setMoved();
                 if (grid.cellMaps.Get(selfCoord.coordinates.X - 1, selfCoord.coordinates.Y + 1, selfCoord.coordinates.Z).state != cellState.None)
                 {
                     if (grid.cellMaps.Get(selfCoord.coordinates.X - 1, selfCoord.coordinates.Y + 1, selfCoord.coordinates.Z).w <= selfCoord.coordinates.W)
@@ -293,13 +270,13 @@ public class PlayerManager : MonoBehaviour
         {
             if (FieldGameManager.Net.isOnline)
             {
-                GameManager.data.setMoved();
+                //GameManager.data.setMoved();
                 // 서버에 이동 전송
                 FieldGameManager.Net.SendMovePacket((byte)Protocol.DIR.DOWN);
             }
             else
             {
-                GameManager.data.setMoved();
+                //GameManager.data.setMoved();
                 if (grid.cellMaps.Get(selfCoord.coordinates.X, selfCoord.coordinates.Y + 1, selfCoord.coordinates.Z - 1).state != cellState.None)
                 {
                     if (grid.cellMaps.Get(selfCoord.coordinates.X, selfCoord.coordinates.Y + 1, selfCoord.coordinates.Z -1).w <= selfCoord.coordinates.W)
@@ -314,13 +291,13 @@ public class PlayerManager : MonoBehaviour
         {
             if (FieldGameManager.Net.isOnline)
             {
-                GameManager.data.setMoved();
+                //GameManager.data.setMoved();
                 // 서버에 이동 전송
                 FieldGameManager.Net.SendMovePacket((byte)Protocol.DIR.RIGHTDOWN);
             }
             else
             {
-                GameManager.data.setMoved();
+                //GameManager.data.setMoved();
                 if (grid.cellMaps.Get(selfCoord.coordinates.X + 1, selfCoord.coordinates.Y, selfCoord.coordinates.Z - 1).state != cellState.None)
                 {
                     if (grid.cellMaps.Get(selfCoord.coordinates.X + 1, selfCoord.coordinates.Y, selfCoord.coordinates.Z - 1).w <= selfCoord.coordinates.W)
@@ -332,39 +309,5 @@ public class PlayerManager : MonoBehaviour
             }
         }
 
-    }
-
-    void resetPosition()
-    {
-        //gameObject.transform.position = new Vector3(gameObject.transform.position.x, 0f, gameObject.transform.position.z);
-
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x, (2f * GameManager.data.beatCounter / GameManager.data.timeByBeat) - 0.1f, gameObject.transform.position.z);
-    }
-
-    public void SetBallBeat(Beat now,Beat b)
-    {
-        Debug.Log(GameManager.data.timeByBeat / (float)b.GetBeatTime() * 0.75f);
-        SettledBallBeats.Add((now + b, GameManager.data.timeByBeat / (float)b.GetBeatTime() * 0.75f));
-    }
-    public void BallBeatCheck()
-    {
-        if (SettledBallBeats.Count > 0)
-        {
-            if (SettledBallBeats[0].Item1.GetBeatTime() + GameManager.data.JudgementTiming < GameManager.data.nowBeat.GetBeatTime())
-            {
-                //놓침
-                Debug.Log("반격 실패");
-                VFXManager.data.HitSounder(SettledBallBeats[0].Item2);
-                SettledBallBeats.RemoveAt(0);
-            }
-            else if (SettledBallBeats[0].Item1.GetBeatTime() - GameManager.data.JudgementTiming < GameManager.data.nowBeat.GetBeatTime()
-                && playerAttacking)
-            {
-                //공격
-                Debug.Log("반격 성공!");
-                VFXManager.data.HitSounder(1);
-                SettledBallBeats.RemoveAt(0);
-            }
-        }
     }
 }
