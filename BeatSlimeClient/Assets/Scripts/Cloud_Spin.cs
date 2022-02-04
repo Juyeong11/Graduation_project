18dc2b5e07c0065d88c130ac9ef04bdb3b34d33e
SH-Broar / Ephxod <ephxod01@naver.com>
2022-02-04 14:12:39 +0900

    Cloud Spin Adapt

--- BeatSlimeClient/Assets/Scripts/Cloud_Spin.cs
+++ BeatSlimeClient/Assets/Scripts/Cloud_Spin.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Cloud_Spin : MonoBehaviour
 {
-
-    public float degree = 0.0;
+    public float degree = 0.0f;
     // Start is called before the first frame update
     
     // Update is called once per frame
     void Update()
     {
-  
+        gameObject.transform.Rotate(new Vector3(0, degree, 0));
     }
 }
