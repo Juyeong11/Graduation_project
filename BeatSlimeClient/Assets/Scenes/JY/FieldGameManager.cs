e52caeeffa8320f5d4ecfe001a18235b33e64b7e
SH-Broar / Ephxod <ephxod01@naver.com>
2022-04-25 01:23:52 +0900

    Fix Gray Portal Turbulance

--- BeatSlimeClient/Assets/Scenes/JY/FieldGameManager.cs
+++ BeatSlimeClient/Assets/Scenes/JY/FieldGameManager.cs
@@ -447,14 +447,13 @@ public class FieldGameManager : MonoBehaviour
 
     IEnumerator ChangeScene()
     {
+        yield return new WaitForSeconds(0.5f);
         player.GetComponentInParent<FieldPlayerManager>().PortalPlane.transform.SetParent(null);
+        player.GetComponentInParent<FieldPlayerManager>().PortalPlane.transform.eulerAngles = new Vector3(0, 0, 0);
         //player.GetComponent<FieldPlayerManager>().PortalPlane.transform.localRotation = Quaternion.Euler(90,0,0);
         player.GetComponentInParent<FieldPlayerManager>().PortalPlane.SetActive(true);
 
-
-
-
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(1.5f);
         player.GetComponentInParent<FieldPlayerManager>().EnterPortal();
         yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene(scene_num);
