ec3fcf83694a4f6f1fa06c9e0ba4d387cfd5e605
SH-Broar / Ephxod <ephxod01@naver.com>
2022-05-03 19:52:48 +0900

    Login Scene

--- BeatSlimeClient/Assets/TransparentSliding.cs
+++ BeatSlimeClient/Assets/TransparentSliding.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class TransparentSliding : MonoBehaviour
 {
+    public RectTransform self;
     public Material mat;
     private float tr;
 
-    void Start() {
-        tr = mat.GetFloat("_TransparentPower");
+    void Start()
+    {
+        self.localScale = new Vector3(Screen.width / 1920f, 1, 1);
+        //self.rect.Set(self.rect.x, self.rect.y, Screen.width/4f, self.rect.height);
+        //print(self.rect.xMin + " " + self.rect.yMin + " " +  Screen.width/4f + " " + self.rect.height);
+        //tr = mat.GetFloat("_TransparentPower");
     }
     
     void Update()
     {
-        tr = 0.3f*Mathf.Sin(Time.time) + 1.05f;
-        mat.SetFloat("_TransparentPower", tr);
+        // tr = 0.3f*Mathf.Sin(Time.time) + 1.05f;
+        // mat.SetFloat("_TransparentPower", tr);
     }
 }
