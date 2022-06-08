8a5d66a65d2f483ab08a670c9b96462e808c42d0
SH-Broar / Ephxod <ephxod01@naver.com>
2022-06-08 15:37:09 +0900

    Fix Shop Bug - Bizarre Click, Wrong level and price

--- BeatSlimeClient/Assets/Prefabs/Shop/ShopManager.cs
+++ BeatSlimeClient/Assets/Prefabs/Shop/ShopManager.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
+
 public class ShopManager : MonoBehaviour
 {
+    const int SKILL_MAX_LEVEL = 3;
     public ShopPrices shopPrices;
 
     public TMP_Text[] skill_price;
@@ -13,23 +16,70 @@ public class ShopManager : MonoBehaviour
     private int selectedskillNum;
     void Update()
     {
-        skill_price[0].text = shopPrices.Skill1Prices[FieldPlayerManager.skillLevelsContainer[0]+1].ToString();
-        skill_price[1].text = shopPrices.Skill2Prices[FieldPlayerManager.skillLevelsContainer[1]+1].ToString();
-        skill_price[2].text = shopPrices.Skill3Prices[FieldPlayerManager.skillLevelsContainer[2]+1].ToString();
-        skill_level[0].text = "Lv. " + (FieldPlayerManager.skillLevelsContainer[0]+1);
-        skill_level[1].text = "Lv. " + (FieldPlayerManager.skillLevelsContainer[1]+1);
-        skill_level[2].text = "Lv. " + (FieldPlayerManager.skillLevelsContainer[2]+1);
+        if (FieldPlayerManager.skillLevelsContainer[0]==SKILL_MAX_LEVEL)
+        {
+            skill_price[0].text = " ";
+            skill_level[0].text = "LV. MAX";
+        }
+        else
+        {
+            skill_price[0].text = shopPrices.Skill1Prices[FieldPlayerManager.skillLevelsContainer[0]+1].ToString();
+            skill_level[0].text = "Lv. " + (FieldPlayerManager.skillLevelsContainer[0]+1);
+        }
+        if (FieldPlayerManager.skillLevelsContainer[1] == SKILL_MAX_LEVEL)
+        {
+            skill_price[0].text = " ";
+            skill_level[1].text = "LV. MAX";
+        }
+        else
+        {
+            skill_price[1].text = shopPrices.Skill2Prices[FieldPlayerManager.skillLevelsContainer[1]+1].ToString();
+            skill_level[1].text = "Lv. " + (FieldPlayerManager.skillLevelsContainer[1] + 1);
+        }
+        if (FieldPlayerManager.skillLevelsContainer[2] == SKILL_MAX_LEVEL)
+        {
+            skill_price[0].text = " ";
+            skill_level[2].text = "LV. MAX";
+        }
+        else
+        {
+            skill_price[2].text = shopPrices.Skill3Prices[FieldPlayerManager.skillLevelsContainer[2]+1].ToString();
+            skill_level[2].text = "Lv. " + (FieldPlayerManager.skillLevelsContainer[2] + 1);
+        }
     }
 
     public void ShopOpen()
     {
-
-        skill_price[0].text = shopPrices.Skill1Prices[FieldPlayerManager.skillLevelsContainer[0]+1].ToString();
-        skill_price[1].text = shopPrices.Skill2Prices[FieldPlayerManager.skillLevelsContainer[1]+1].ToString();
-        skill_price[2].text = shopPrices.Skill3Prices[FieldPlayerManager.skillLevelsContainer[2]+1].ToString();
-        skill_level[0].text = "Lv. " + (FieldPlayerManager.skillLevelsContainer[0]+1);
-        skill_level[1].text = "Lv. " + (FieldPlayerManager.skillLevelsContainer[1]+1);
-        skill_level[2].text = "Lv. " + (FieldPlayerManager.skillLevelsContainer[2]+1);
+        if (FieldPlayerManager.skillLevelsContainer[0]==SKILL_MAX_LEVEL)
+        {
+            skill_price[0].text = " ";
+            skill_level[0].text = "LV. MAX";
+        }
+        else
+        {
+            skill_price[0].text = shopPrices.Skill1Prices[FieldPlayerManager.skillLevelsContainer[0]+1].ToString();
+            skill_level[0].text = "Lv. " + (FieldPlayerManager.skillLevelsContainer[0]+1);
+        }
+        if (FieldPlayerManager.skillLevelsContainer[1] == SKILL_MAX_LEVEL)
+        {
+            skill_price[0].text = " ";
+            skill_level[1].text = "LV. MAX";
+        }
+        else
+        {
+            skill_price[1].text = shopPrices.Skill2Prices[FieldPlayerManager.skillLevelsContainer[1]+1].ToString();
+            skill_level[1].text = "Lv. " + (FieldPlayerManager.skillLevelsContainer[1] + 1);
+        }
+        if (FieldPlayerManager.skillLevelsContainer[2] == SKILL_MAX_LEVEL)
+        {
+            skill_price[0].text = " ";
+            skill_level[2].text = "LV. MAX";
+        }
+        else
+        {
+            skill_price[2].text = shopPrices.Skill3Prices[FieldPlayerManager.skillLevelsContainer[2]+1].ToString();
+            skill_level[2].text = "Lv. " + (FieldPlayerManager.skillLevelsContainer[2] + 1);
+        }
 
         gameObject.SetActive(true);
     }
@@ -41,6 +91,11 @@ public class ShopManager : MonoBehaviour
     
     public void BuySkill(int skillNum)
     {
+        if (FieldPlayerManager.skillLevelsContainer[skillNum] == SKILL_MAX_LEVEL)
+        {
+            return;
+        }
+        //Debug.Log($"Skill Buy : {skillNum}");
         Network.SendBuyPacket((byte)((skillNum-1)*4+FieldPlayerManager.skillLevelsContainer[skillNum-1]+1));
     }
 }
