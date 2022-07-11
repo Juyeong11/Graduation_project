086c48c53691d922ce218ede2b5aa0597a07265f
SH-Broar / Ephxod <ephxod01@naver.com>
2022-07-11 18:16:34 +0900

    add SendTutorial

--- BeatSlimeClient/Assets/Scripts/Login/LoginManager.cs
+++ BeatSlimeClient/Assets/Scripts/Login/LoginManager.cs
@@ -58,4 +58,35 @@ public class LoginManager : MonoBehaviour
         
         Network.SendLogIn(id);
     }
+
+    public void SendTutorial()
+    {
+         string id = ID.text;//.Remove(ID.text.Length-1,1);
+
+        string idChecker = Regex.Replace(id, @"[^a-zA-Z0-9]{1,20}", "", RegexOptions.Singleline);
+        id = id.Remove(ID.text.Length - 1, 1);
+
+        if (id == "_MAPMAKER")
+        {
+            SceneManager.LoadScene("MapMakingScene");
+            return;
+        }
+
+        if (id.Equals(idChecker) == false) { 
+            print("잘못된 아이디 형식입니다 형식에 맞춰 다시 작성해 주세요(특수 문자 사용 불가능, 글자 수 20이하)"); 
+            print(id.Length);
+            print(id);
+            print(idChecker.Length);
+            print(idChecker);
+            return; 
+        }
+
+        
+       
+        SceneManager.LoadScene("InGameScene01");
+        
+        //SceneManager.LoadScene("FieldScene");
+        
+        //Network.SendLogIn(id);
+    }
 }
